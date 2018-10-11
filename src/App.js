3254eaf4a8ee261cf0a09e5b015c38a6cba19f66
Aleksei Merezhko <aleksei.merezhko@mail.ru>
2018-10-11 18:36:43 +0300

    react-router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+//Import Pages
+import Coins from './pages/coins/Coins';
+import Converter from './pages/converter/Converter';
+import History from './pages/history/History';
+import News from './pages/news/News';
+import Home from './pages/home/Home'; 
+
+import ForOFor from './pages/404';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  list(){
-    const li = document.querySelectorAll(`li`);
-    li.forEach(param => param.innerHTML = `React`);
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>My component</h1>
-        <h2>Title</h2>
-        <img src={logo} className="App-logo" onClick={this.list} alt="logo"/>
-        <div className="box">
-          <ul>
-            <li className="first">Click</li>
-            <li className="second">on</li>
-            <li className="third">React</li>
-            <li className="fourth">Ico</li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+     <header>
+       <div className="App-logo">
+         <img src={logo} alt="Crypto"/>
+       </div>
+      <ul>
+        <li>
+          <NavLink to="/" activeClassName="active" exact>Price</NavLink>
+        </li>
+        <li>
+          <NavLink to="/history" activeClassName="active">History</NavLink>
+        </li>
+        <li>
+          <NavLink to="/converter" activeClassName="active">Converter</NavLink>
+        </li>
+        <li>
+          <NavLink to="/news" activeClassName="active">News</NavLink>
+        </li>
+        <li>
+          <NavLink to="/coins" activeClassName="active">Coins</NavLink>
+        </li>
+      </ul>
+      
+     </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/history" component={History} />
+        <Route path="/converter" component={Converter} />
+        <Route path="/news" component={News} />
+        <Route path="/coins" component={Coins} />
+        <Route component={ForOFor} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+App.displayName = 'App';
 
 export default App;
